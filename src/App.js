12b55677e4921538eb737f43db2b1c6eb6d84344
hossein <hossein@yahoo.com>
2025-07-25 16:48:20 +0330

    commitTwo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Blog from './pages/blog/Bolg'
+import Course from './pages/cours/Cours'
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route />
+                    <Route path='/' element= { <Home />} />
+                    <Route path='/about' element= { <About /> } />
+                    <Route path='/blog' element= { <Blog />} />
+                    <Route path='/cours' element= { <Course />} />
                 </Routes>
             </BrowserRouter>
         </>
--- src/pages/curs/Cours.js
+++
@@ -1,6 +0,0 @@
-function Course(){
-    return(
-        <h1>Course page</h1>
-    )
-}
-export default Course
